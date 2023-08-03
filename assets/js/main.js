4c028838a9ba18cda4c618c089dbc64cf6423491
Dario Urdaneta <dariourdaneta@gmail.com>
2023-08-03 11:48:27 -0400

    adding solid principles to variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,23 @@
 /* Variables */
-
 let userQuantity = document.getElementById('quantity');
 let userColor = document.getElementById('color');
 
 /* Constantes */
 const PRODUCT_PRICE = 19990;
-const totalPrice = document.getElementById('total');
-const finalQuantity = document.getElementById('qty-final');
-const finalColor = document.getElementById('user-color');
+const FINAL_PRICE = document.getElementById('total');
+const FINAL_QUANTITY = document.getElementById('qty-final');
+const FINAL_COLOR = document.getElementById('user-color');
 
 /* Formato precio CLP */
-
-totalPrice.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "CLP" }).format(PRODUCT_PRICE);
+FINAL_PRICE.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "CLP" }).format(PRODUCT_PRICE);
 
 /* Funcion para totalizar */
-
 const calculator = (event) => {
   event.preventDefault();
   let totalCalculator = userQuantity.value * PRODUCT_PRICE;
-  totalPrice.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "CLP" }).format(totalCalculator);
-  finalQuantity.innerHTML = userQuantity.value;
-  finalColor.style.backgroundColor = userColor.value;
+  FINAL_PRICE.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "CLP" }).format(totalCalculator);
+  FINAL_QUANTITY.innerHTML = userQuantity.value;
+  FINAL_COLOR.style.backgroundColor = userColor.value;
 };
 
 
