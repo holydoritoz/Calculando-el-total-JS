2cf8b6c5f5dad09f2bcad4fa4bb98f594a109fbd
Dario Urdaneta <dariourdaneta@gmail.com>
2023-08-02 21:17:20 -0400

    adding arrow funtions

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,12 @@ const finalColor = document.getElementById('user-color');
 total.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "CLP" }).format(PRODUCT_PRICE);
 
 
-
-function calculator(event) {
+const calculator = (event) => {
   event.preventDefault();
-  let totalCalculator = (userQuantity.value) * PRODUCT_PRICE;
+  let totalCalculator = userQuantity.value * PRODUCT_PRICE;
   total.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "CLP" }).format(totalCalculator);
   finalQuantity.innerHTML = userQuantity.value;
   finalColor.style.backgroundColor = userColor.value;
-}
+};
+
 
